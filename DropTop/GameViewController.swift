247bc78c38e4980a9d8d4dda68192b9490ddd225
Mnpn <mnpn03@gmail.com>
2018-07-30 10:53:24 +0200

    SimpleVolume (CocoaPods)

--- DropTop/GameViewController.swift
+++ DropTop/GameViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import SubtleVolume
 
 class GameViewController: UIViewController {
     // Create a UIScreenEdgePanGestureRecognizer.
     var screenEdgeRecognizer: UIScreenEdgePanGestureRecognizer!
+    
+    let volume = SubtleVolume(style: .plain)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +36,18 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        var volumeOrigin: CGFloat = UIApplication.shared.statusBarFrame.height
+        if #available(iOS 11.0, *) {
+            volumeOrigin = view.safeAreaInsets.top
+        }
+        volume.frame = CGRect(x: 0, y: volumeOrigin, width: UIScreen.main.bounds.width, height: 4)
+        volume.barTintColor = .white
+        volume.barBackgroundColor = volume.barTintColor.withAlphaComponent(0.3)
+        volume.animation = .fadeIn
+        view.addSubview(volume)
     }
     
     override var shouldAutorotate: Bool {
